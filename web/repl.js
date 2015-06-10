6b57e4d5f9e6eb177eb3aa83cdd8427a79c78878
Ramil Farkhshatov <ramil@gmx.co.uk>
2015-06-10 11:10:05 +0300

    Pulled changes from master

--- web/repl.js
+++ web/repl.js
@@ -1,11 +1,13 @@
 shen_web.init_repl = function() {
   shen_web.set_init_status("Initializing repl");
-  var repl = {};
+  var line_buffer = [],
+      buffer_index = 0,
+      repl = {};
   function puts(str, tag) {
-    var cont = repl.out.parentNode;
-    var sd = cont.scrollHeight - cont.scrollTop;
-    var diff = Math.abs(sd - cont.clientHeight);
-    var t = document.createTextNode(str);
+    var cont = repl.out.parentNode,
+        sd = cont.scrollHeight - cont.scrollTop,
+        diff = Math.abs(sd - cont.clientHeight),
+        t = document.createTextNode(str);
     if (tag) {
       var s = document.createElement("span");
       s.className = "repl_tag_" + tag;
@@ -37,10 +39,26 @@ shen_web.init_repl = function() {
       var key = e.keyCode || e.which;
       if (key == 0xd && !e.ctrlKey) {
         var line = repl.inp.textContent || repl.inp.innerText;
-        line = line.trimRight("\n") + "\n";
-        shen_web.send(line);
+        line = line.trimRight("\n");
+        shen_web.send(line + "\n");
         shen_web.clean(repl.inp);
+
+        if (line_buffer.length == 0
+            || line != line_buffer[line_buffer.length - 1]) {
+          line_buffer.push(line);
+        }
+        buffer_index = line_buffer.length;
         return true;
+      } else if (key == 0x26) {
+        if (buffer_index > 0) {
+          var new_text = line_buffer[--buffer_index];
+          repl.inp.textContent = new_text;
+        }
+      } else if (key == 0x28) {
+        if (buffer_index < line_buffer.length) {
+          buffer_index++;
+          repl.inp.textContent = line_buffer[buffer_index];
+        }
       }
       return false;
     };
